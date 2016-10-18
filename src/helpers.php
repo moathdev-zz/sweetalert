0397ecabd4b63d535509b96a19995f11b0b75a2c
Moath Aljehany <M.Aljehany@tamkeentech.sa>
2016-10-18 09:11:40 +0300

    bug fix

--- src/helpers.php
+++ src/helpers.php
@@ -1,16 +1,12 @@
 <?php
 
-/**
- * @param $title
- * @param $message
- * @param $type
- * @param bool|true $showConfirmButton
- * @return mixed
- */
-function flash($title, $message, $type, $showConfirmButton = true)
-{
-    $flash = app('Moathdev\Sweetalert\Flash');
+if(!function_exists ('flash')) {
 
-    return $flash->message($title, $message, $type, $showConfirmButton);
+    function flash($title, $message, $type, $showConfirmButton = true)
+    {
+        $flash = app('Moathdev\Sweetalert\Flash');
 
+        return $flash->message($title, $message, $type, $showConfirmButton);
+
+    }
 }
